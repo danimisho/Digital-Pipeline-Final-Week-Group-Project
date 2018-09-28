41705efb72de8808ec67508c444f6f04a0cf5c56
Danny Michaux <dannymichaux@gmail.com>
2018-09-28 14:44:08 +0100

    added files

---
+++ js/main.js
@@ -0,0 +1,39 @@
+console.log('it works');
+
+var emergencyCallAudio = new Audio('../audio/999call.mp3');
+
+$(document).ready(()=>{
+  $('.call-button').on('click', ()=>{
+    console.log('click');
+    emergencyCallAudio.play();
+  })
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
